e4e840a873f1c1928c77566b0fbb3136a5428dbc
LaureSaintAntonin <lsaint_antonin@yahoo.fr>
2023-12-15 15:45:30 +0100

    reborn4bis - Server tjs fonctionnel

--- src/mysite/loventoutou/views.py
+++ src/mysite/loventoutou/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse("Bienvenue sur TinderToutou")
+	return HttpResponse("Bienvenue sur LovenToutou")
+
+def user(request):
+	return HttpResponse("Connectez-vous")
 
 # Create your views here.
