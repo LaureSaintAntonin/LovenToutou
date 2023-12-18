198d78b49a0cdd20248b610e7f86fb07dc198d02
LaureSaintAntonin <lsaint_antonin@yahoo.fr>
2023-12-18 15:29:05 +0100

    rajout des chemins urls pour les views index et connexion

--- src/mysite/loventoutou/urls.py
+++ src/mysite/loventoutou/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 # La fonction path() reçoit quatre paramètres,
 # dont deux sont obligatoires : route et view, et deux facultatifs : kwargs et name.
+app_name = "loventoutou"
 urlpatterns = [
-	path("", views.index, name="index"),
-	path("", views.connexion, name="connexion"),
+	path("", views.index, name="index"), 
+	path("connexion/", views.connexion, name="connexion"),
 	path("", views.user, name="user"),
 	path("", views.navigation, name="navigation"),
 ]
+
+# index et connexion fonctionnent, ne pas modifier !
