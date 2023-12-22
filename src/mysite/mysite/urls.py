14835f8d80c369cced5ba2d18fee72a3d7d03a78
LaureSaintAntonin <lsaint_antonin@yahoo.fr>
2023-12-22 16:09:20 +0100

    tests urls

--- src/mysite/mysite/urls.py
+++ src/mysite/mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("loventoutou/", include("loventoutou.urls")),
+    # path ("loventoutou/connexion/", include("loventoutou.urls"))
 ]
